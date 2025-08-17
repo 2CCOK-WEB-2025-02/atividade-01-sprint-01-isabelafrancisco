341b023df8faebe5c6232acb52f61566671269aa
isabelafrancisco <isabela.francisco@sptech.school>
2025-08-17 12:24:24 -0300

    testes rodando

--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -8,7 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+
+        if (n <= 0) {
+            return new ExercicioDificilResponse(0, 0);
+        }
+
+        int a = 1;
+        int b = 1;
+        int soma = 1;
+
+        if (n == 1) {
+            return new ExercicioDificilResponse(1, soma);
+        }
+
+        soma += b;
+        int enesimo = b;
+
+        for (int i = 3; i <= n; i++) {
+            enesimo = a + b;
+            soma += enesimo;
+            a = b;
+            b = enesimo;
+        }
+
+        return new ExercicioDificilResponse(enesimo, soma);
     }
 }
